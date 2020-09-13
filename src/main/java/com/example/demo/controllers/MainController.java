7977d9de037705d89d0274263a2adf95d8489372
dulatSagimbayev <dulatSagimbayev@users.noreply.github.com>
2020-09-13 18:30:37 +0600

    initial commit

---
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+    @GetMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title", "Страница о нас");
+        return "about";
+    }
+    @GetMapping("/login")
+    public String login(Model model){
+        return "login";
+    }
+
+}
